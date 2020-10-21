6c91b0fe8e14786f18cb45b4d76c7712210da7a8
Liao, Zhaojian <72433128+acse-zl620@users.noreply.github.com>
2020-10-21 18:45:22 +0800

    no

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,9 +1,11 @@
 from functools import lru_cache
-__all__ = ['my_sum', 'lru_cache']
+__all__ = ['my_sum', 'lru_cache', 'factorial']
+
 
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
 def factorial(n):
     return n * factorial(n-1) if n else 1
+
 
 def my_sum(iterable):
     tot = 0
--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -4,14 +4,14 @@ from simple_functions import my_sum
 
 
 class TestSimpleFunctions(object):
-    '''Class to test our simple functions are working correctly'''
+    """Class to test our simple functions are working correctly"""
 
     @pytest.mark.parametrize('iterable, expected', [
         ([8, 7, 5], 20),
         ((10, -2, 5, -10, 1), 4)
     ])
     def test_my_add(self, iterable, expected):
-        '''Test our add function'''
+        """Test our add function"""
         isum = my_sum(iterable)
         assert isum == expected
 
@@ -21,6 +21,6 @@ class TestSimpleFunctions(object):
         (1, 1)
     ])
     def test_factorial(self, number, expected):
-        '''Test our factorial function'''
+        """Test our factorial function"""
         answer = factorial(number)
         assert answer == expected
